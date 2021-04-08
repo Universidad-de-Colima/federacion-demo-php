05c6cab637a95365d24d9521232568e874bd5a31
sistemas-ucol <gcruz@ucol.mx>
2021-04-08 11:14:09 -0500

    Correccion de acentos
    test

--- config.php
+++ config.php
@@ -1,12 +1,12 @@
 <?php 
     //header('Content-Type: text/html; charset=iso-8859-1');
-    //La ruta donde se encuentra la librería principal de simplesamlphp
+    //La ruta donde se encuentra la libreroa principal de simplesamlphp
 	$saml_lib_path = '/path/to/simplesamlphp/lib/_autoload.php';  
     require_once($saml_lib_path);
 	// url de nuestro servidor, en este caso, carpeta demo.
     $SP_URL = 'https://'.$_SERVER['SERVER_NAME']."/demo/";  
-    // Fuente de autenticación definida en el authsources del SP ej, default-sp
+    // Fuente de autenticacion definida en el authsources del SP ej, default-sp
 	$SP_ORIGEN= 'desarrollo4sistemas';   
-    // Se crea la instancia del saml, pasando como parametro la fuente de autenticación.
+    // Se crea la instancia del saml, pasando como parametro la fuente de autenticacion.
 	$saml = new SimpleSAML_Auth_Simple($SP_ORIGEN);   
 ?>
--- index.php
+++ index.php
@@ -6,5 +6,5 @@
 	  echo "<br> Existe sesi&oacute;n a nombre de ".$atributos["uNombre"][0]."<br><a href='./privada/'>Ir a secci&oacute;n privada</a>"; //Imprimir el atributo uNombre
        } 
 	else 
-	 echo "<br>No hay sesión iniciada<br><a href='./privada/'>Iniciar sesi&oacute;n</a>";
+	 echo "<br>No hay sesi&oacute;n iniciada<br><a href='./privada/'>Iniciar sesi&oacute;n</a>";
 ?>
